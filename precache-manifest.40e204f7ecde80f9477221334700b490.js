d17e1a1b035c0e3f1df6b997fdd409457453bcaf
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-02 13:01:39 -0800

    Updates

---
+++ precache-manifest.40e204f7ecde80f9477221334700b490.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7c1524d2a3e8e9131f0",
+    "url": "/rick-morty/static/css/main.9b710c1c.chunk.css"
+  },
+  {
+    "revision": "e7c1524d2a3e8e9131f0",
+    "url": "/rick-morty/static/js/main.af9e4e05.chunk.js"
+  },
+  {
+    "revision": "a079f2fc39b5e73e24f1",
+    "url": "/rick-morty/static/js/runtime~main.6bffb73a.js"
+  },
+  {
+    "revision": "0f195abedd5f7ef66ebf",
+    "url": "/rick-morty/static/js/2.eedbfbed.chunk.js"
+  },
+  {
+    "revision": "ebccfced1319a21a09a56564e212ae28",
+    "url": "/rick-morty/index.html"
+  }
+];
